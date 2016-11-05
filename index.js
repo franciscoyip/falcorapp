d8b28988e65bfe4268faa54d6b0f6bb46b0c78f9
Francisco Yip <francisco.yip@gmail.com>
2016-11-04 21:36:59 -0700

    initial setup commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = React.createClass({
+  render() {
+    return (
+      <h1></h1>
+    );
+  }
+});
+
+
+ReactDOM.render(<App/>, document.getElementById('target'));
